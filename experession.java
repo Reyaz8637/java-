c2e8b351aa456a0e8491ad114abff5c795b06d20
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:50:55 +0530

    Add files via upload

---
+++ experession.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+class experession{
+    public static void main(String arg[]){
+     float height,base,area;
+     System.out.println("enter height and base ");
+     Scanner sc=new Scanner(System.in);
+     base=sc.nextFloat();
+     height=sc.nextFloat();
+     area=base*height/2;
+     System.out.println("area of triangle"+area);}
+
+    }            
