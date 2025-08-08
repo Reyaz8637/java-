762b90ca44a51d278943dd58306b4d1ab325ccac
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:47:47 +0530

    Add files via upload

---
+++ datasizerange.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+ class datasizerange{
+	public static void main(String arg[]){
+	System.out.println("Short Min :"+Short.MIN_VALUE);
+	System.out.println("Short Max :"+Short.MAX_VALUE);
+	System.out.println("Short Bytes :"+Short.BYTES);}
+}
