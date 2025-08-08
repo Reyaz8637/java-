26696794203e241551fee44eaa574934a2bbfe16
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:46:50 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,15 @@
+public class conditional {
+    public static void main(String args[]){
+        int a=5,b=15,c=10;
+        if(a>b && b>c){
+            System.out.println(a);
+        }
+        else if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+    
+}
