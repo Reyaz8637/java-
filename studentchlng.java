27d880d8b3ab14dca8da9f39cc1da683f3e5acc7
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 12:02:49 +0530

    Add files via upload

---
+++ studentchlng.java
@@ -0,0 +1,47 @@
+public class studentchlng {
+    public static void main(String[] args) {
+        // String str="programmer@gmail.com";
+        // int i=str.indexOf("@");
+        // System.out.println(i);
+        // String uname=str.substring(0,i);
+        // System.out.println("username:"+uname);
+        // String domain=str.substring(i+1,str.length());
+        // System.out.println("domain name:"+domain);
+        // System.out.println(domain.startsWith("gmail"));
+
+        // "BINARY NUMBER CHECKING"
+
+        // int b=10110001;
+        // String str=b+"";
+        // System.out.println(str.matches("[01]+"));
+
+        // "HEXADECIMAL NUMBER CHECKING"
+
+        // String str="1234AB";
+        // System.out.println(str.matches("[0-9A-F]+"));
+
+        // DATE FORMATE
+        // String str="24/10/2024";
+        // System.out.println(str.matches("[0-3][0-9]/[0-1][0-9]/[0-9]{4}"));
+
+        // "REMOVAL OF SPECIAL CHARACTER FROM STRING"
+        // String str="a&b$c%C#2D*";
+        // System.out.println(str.replaceAll("[^a-zA-Z0-9]", ""));
+
+        // "REMOVAL OF EXTRA SPACES"
+        // String str="abc   efg   ijkl  nopqrstr";
+        // System.out.println(str.replaceAll("\\s+"," "));
+
+        // "NUMBER OF WORDS"
+        // String str="    abc    def    ijkhk    ";
+        // System.out.println(str.replaceAll("\\s+"," ").trim());
+        // String words[]=str.split("\\s");
+        // System.out.println(words.length);
+        // String a="abc";
+        // String b=new String("abc");
+        // System.out.println(a.equals(b));
+        // System.out.println(a==b);
+
+    }
+    
+}
