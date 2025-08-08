26f6c1f07c779cd15371256c1a2ea9bd70dc4b47
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:54:18 +0530

    Add files via upload

---
+++ litrals.java
@@ -0,0 +1,14 @@
+
+class literals
+{
+	public static void main(String arg[]){
+	byte b1=10;
+	byte b2=0b1010;
+	byte b3=012;
+	byte b4=0XA;
+	System.out.println(b1);
+	System.out.println(b2);
+	System.out.println(b3);
+	System.out.println(b4);
+}
+}
