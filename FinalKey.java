bd82ba99a5637747d5b4f354b3955c4038733baf
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:51:20 +0530

    Add files via upload

---
+++ FinalKey.java
@@ -0,0 +1,17 @@
+// public class FinalKey {
+//  final float PI;
+//      public FinalKey(){
+//         PI=3.1423f;}
+     
+//     public static  void main(String[] args) {
+        
+//     }
+// }
+
+//  DEMO//
+final class test{
+    public final void show(){System.out.println("show");}
+}
+class test1 extends test{
+    
+} 
