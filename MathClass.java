64b1ffc7ee20093b059afe21003244cfcb6f256e
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:55:42 +0530

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,58 @@
+public class MathClass {
+    public static void main(String[] args) {
+        // System.out.print("Absolute :");
+        // System.out.println(Math.abs(-123));
+
+        // System.out.print("Absolute :");
+        // System.out.println(StrictMath.abs(-123));
+
+        // System.out.print("cube Root :");
+        // System.out.println(Math.cbrt(27));
+
+        // System.out.print("Excat Decrement :");
+        // System.out.println(Math.decrementExact(7));
+
+    //     System.out.print("Exponent value in floating point reprersentation :");
+    //     System.out.println(Math.getExponent(12.345));
+
+        // System.out.print("Round Division :");
+        // System.out.println(Math.floorDiv(50,9));
+
+        // System.out.print("e poewer x:");
+        // System.out.println(Math.exp(1));
+
+        // System.out.print("e power x:");
+        // System.out.println(StrictMath.exp(1));
+
+        // System.out.print("log base 10 :");
+        // System.out.println(Math.log10(100));
+
+        // System.out.print("maximum :");
+        // System.out.println(Math.max(10,9));
+
+        // System.out.print("minimum :");
+        // System.out.println(Math.min(10,9));
+
+        // System.out.println("tan :");
+        // System.out.println(Math.tan(45*Math.PI/180));
+
+    //     System.out.print("convert to radian :");
+    //     System.out.println(Math.toRadians(45));
+
+    //     System.out.print("convert to degree :");
+    //     System.out.println(Math.toDegrees(Math.atan(0.7853981633974483)));
+
+    System.out.print("Random number :"); 
+    System.out.println(Math.random()*1000);
+
+    System.out.print("Power :");
+    System.out.println(Math.pow(2, 3));
+
+    System.out.print("Exact product :");
+    System.out.println(Math.multiplyExact(100,200));
+
+    System.out.print("Next float value :");
+    System.out.println(Math.nextAfter(12.5, 13));
+}
+}
+
---
+++ member.java
@@ -0,0 +1,48 @@
+// interface Test{
+//     void meth1();
+//     void meth2();
+// }
+// class Test2 implements Test{
+//     public void meth1(){System.out.println("meth1");}
+//     public void meth2(){System.out.println("meth2");}
+//     public void meth3(){System.out.println("meth3");}
+// }
+// public class interFace {
+//     public static void main(String[] args) {
+//         Test2 t=new Test2();
+//         t.meth1();
+//         t.meth2();
+//         t.meth3();
+        
+//     }
+// }
+
+// DEMO//
+// class phone{
+//    public void sms(){System.out.println("Send sms");}
+//    public void call(){System.out.println("send call");}}
+// interface ICamera{
+//     void click();
+//     void record();}
+// interface Imusicplayer{
+//     void play();
+//     void stop();}
+// class smartphone extends phone implements ICamera,Imusicplayer{
+//     public void videocall(){System.out.println("smart phone vide call");}
+//     public void click(){System.out.println("smartphone click photo");}
+//     public void record(){System.out.println("smartphone record video");}
+//     public void play(){System.out.println("smartphone Play music");}
+//     public void stop(){System.out.println("smartphone stop play");}}
+// class interFace{
+//     public static void main(String[] args) {
+//         smartphone p=new smartphone();
+//         p.call();
+//         p.sms();
+//         p.click();
+//         p.record();
+//         p.play();
+//         p.stop();
+
+//     }
+// }
+
