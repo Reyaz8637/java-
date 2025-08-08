09bf8e1c2df8e42670acbfea484fa2cef28f9d09
Mohammad Reyaz <165875108+Reyaz8637@users.noreply.github.com>
2025-08-08 11:45:43 +0530

    Add files via upload

---
+++ arry2d.java
@@ -0,0 +1,60 @@
+public class arry2d {
+    public static void main(String arg[]){
+        // int A[][]=new int[5][5];
+        // int B[][]={{1,2,3},{4,5,5},{6,8,9}};
+        // for(int i=0;i<B.length;i++){
+        //     for(int j=0;j<B[0].length;j++){
+        //         System.out.print(B[i][j]+",");
+        //     }System.out.println("");
+        // }
+
+        // "FOR EACH LOOP"
+        // for (int x[] : B) {
+        //     for (int y : x) {
+        //         System.out.print(y+",");
+        //     }System.out.println("");
+        // }
+
+        // JACKED ARRAY//
+        // int A[][];
+        // A=new int[3][];
+        // A[0]=new int[5];
+        // A[1]=new int[3];
+        // A[2]=new int[8];
+        // for(int i=0;i<A.length;i++){
+        //     for(int j=0;j<A[i].length;j++){
+        //         System.out.print(A[i][j]+",");
+        //     }System.out.println();
+        // }
+
+        // "ADDITION OF TWO ARRAY//"
+        // int A[][]={{1,2,3},{1,2,3},{3,4,5}};
+        // int B[][]={{2,3,4},{4,5,6},{3,4,4}};
+        // int C[][]=new int[3][3];
+        // for(int i=0;i<A.length;i++){
+        //     for(int j=0;j<A[i].length;j++){
+        //         System.out.print(A[i][j]+",");
+        //     }System.out.println("");
+        // }System.out.println("");
+        // for(int i=0;i<B.length;i++){
+        //     for(int j=0;j<B[i].length;j++){
+        //         System.out.print(B[i][j]+",");
+        //     }System.out.println("");
+        // }System.out.println("");
+        // for(int i=0;i<3;i++){
+        //     for(int j=0;j<3;j++){
+        //        C[i][j]=A[i][j]+B[i][j];
+        //        System.out.print(C[i][j]+",");
+        //     }System.out.println("");
+        // }
+
+        // "SORTING OF ARRAY"//
+
+        // String arr[]={"java","python","pascal","ada","bridge"};
+        // java.util.Arrays.sort(arr);
+        // for ( String x : arr) {
+        //    System.out.println(x); 
+        // }
+        
+    }
+}
